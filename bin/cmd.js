f97d90b6f8586ea68caea2c84928799fdc843785
qgustavor <qgustavor@users.noreply.github.com>
2020-06-03 09:34:50 -0300

    Many changes
    - Updated MEGAJS to support new URLs... which led to npm warnings;
    - Updated libraries to fix warnings... which led to Standard warnings;
    - Fixed style... which led to me noticing uncommitted changes;
    - Fixed #11 and fixed #12.

--- bin/cmd.js
+++ bin/cmd.js
@@ -14,7 +14,10 @@ https://github.com/qgustavor/megajs-cli/releases/latest`
 
 process.title = 'MEGAJS'
 
-require('yargs')
+// The require('yargs').argv getter have side effects
+// so the ESHint error needs to be ignored.
+
+require('yargs') // eslint-disable-line no-unused-expressions
   .commandDir('../lib/commands')
   .recommendCommands()
   .demandCommand(1, 'You need at least one command before moving on')
